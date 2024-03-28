47a3df05845782ee37337afa87d57b68cb2d2558
Althaf P T <rskk2004@gmail.com>
2024-03-28 13:03:56 +0530

    new change

--- contracts/Tracking.sol
+++ contracts/Tracking.sol
@@ -88,5 +88,10 @@ contract Tracking {
         emit ShipmentDelivered(_sender,_reciever,shipment.deliveryTime);
         emit ShipmentPaid(_sender,_reciever,amount);
     }
-    
+
+    function getShipment(address _sender,uint256 _index) public view returns(address,address,uint256,uint256,uint256,uint256,ShipmentStatus,bool){
+        Shipment memory shipment=shipments[_sender][_index];
+        return (shipment.sender,shipment.reciever,shipment.pickupTime,shipment.deliveryTime,shipment.distance,shipment.price,shipment.status,shipment.isPaid);
+    }
+
 }
